0408e5a6a1546e2b9e7db8efe9e9cfb1d928e1c9
KanwarAfaq <kmafaq786@gmail.com>
2026-07-10 15:18:09 +0800

    andriod app

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+
+CapacitorUpdater.notifyAppReady();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
